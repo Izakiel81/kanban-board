e7d26a79a66005fe791ac2a726cd1bbc93f8e272
Maksym Habinskyi <mgabin348@gmail.com>
2025-08-31 18:21:56 +0200

    Added logo and simple styling to the sidebar

--- src/components/sidebar/SidebarComponent.vue
+++ src/components/sidebar/SidebarComponent.vue
@@ -2,7 +2,11 @@
 
 <template>
   <div class="sidebar">
-    <h1>Kanban board</h1>
+    <header>
+      <img src="../../assets/logo.png" alt="logo" />
+      <h1>Kanban board</h1>
+    </header>
+    <hr />
   </div>
 </template>
 
@@ -12,5 +16,24 @@
   height: 100vh;
   background-color: #f4f4f4;
   padding: 20px;
+
+  font-family: "Poppins", sans-serif;
+}
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 10px;
+  margin-bottom: 20px;
+}
+header img {
+  width: 100px;
+  height: 100px;
+}
+header h1 {
+  font-size: 25px;
+  color: #333;
+  text-align: center;
 }
 </style>
