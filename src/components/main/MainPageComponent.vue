adfefa3e4b46b1f27ca90320ce935615eb60341f
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-01 17:14:21 +0200

    Add board button added

--- src/components/main/MainPageComponent.vue
+++ src/components/main/MainPageComponent.vue
@@ -2,14 +2,18 @@
 <template>
   <div class="container">
     <div class="board"></div>
-    <div class="add-board-container"></div>
+    <div class="add-board-container">
+      <h1>+ Add board</h1>
+    </div>
   </div>
 </template>
 <style scoped>
 .container {
   display: flex;
   align-items: flex-start;
+
   padding: 10px;
+
   gap: 10px;
 }
 .board {
@@ -19,5 +23,35 @@
   min-height: 50px;
 
   border-radius: 8px;
+}
+.add-board-container {
+  display: flex;
+
+  border: 2px dashed #ccc;
+  border-radius: 8px;
+
+  min-width: 230px;
+  min-height: 50px;
+
+  justify-content: center;
+  align-items: center;
+
+  text-align: center;
+
+  user-select: none;
+
+  cursor: pointer;
+}
+.add-board-container:hover {
+  background-color: #cccccc69;
+}
+.add-board-container:active {
+  filter: brightness(90%);
+}
+.add-board-container h1 {
+  color: #ccc;
+
+  font-family: "Poppins", sans-serif;
+  font-size: 20px;
 }
 </style>
