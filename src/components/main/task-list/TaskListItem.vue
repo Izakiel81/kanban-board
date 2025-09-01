150dd38519dd8693675e3992c738d9e3a60c9f92
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-01 20:36:30 +0200

    Added card component

---
+++ src/components/main/task-list/TaskListItem.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <div class="title-container">
+      <h2>
+        <span><button>Title</button></span>
+      </h2>
+    </div>
+    <button>Add card</button>
+  </div>
+</template>
+<style scoped></style>
