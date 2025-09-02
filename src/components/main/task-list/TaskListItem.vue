82a4ac8b098db993c48293964126d5e52fc7b07d
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-02 20:23:15 +0200

    Minor changes

--- src/components/main/task-list/TaskListItem.vue
+++ src/components/main/task-list/TaskListItem.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts"></script>
 <template>
-  <div>
+  <div class="container">
     <div class="title-container">
-      <h2>
-        <span><button>Title</button></span>
+      <h2 class="title">
+        <span><button class="title-button">Title</button></span>
       </h2>
     </div>
     <p>Content</p>
   </div>
 </template>
-<style scoped></style>
+<style scoped>
+.container {
+  padding: 5px;
+  display: flex;
+  flex-flow: wrap column;
+}
+.title-container {
+  display: flex;
+}
+.title {
+  width: 100%;
+}
+.title-button {
+  width: 100%;
+  text-align: left;
+}
+</style>
