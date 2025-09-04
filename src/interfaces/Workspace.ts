26af0f00eed08d73c7f9ce88f28f9af30f9a067b
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-04 14:54:43 +0200

    Added id to the interfaces

--- src/interfaces/Workspace.ts
+++ src/interfaces/Workspace.ts
@@ -1,12 +1,15 @@
 export interface Card {
+  id: string;
   title: string;
   description?: string;
 }
 export interface TaskList {
+  id: string;
   title: string;
   cards?: Card[];
 }
 export interface Workspace {
+  id: string;
   title: string;
   taskLists?: TaskList[];
 }
