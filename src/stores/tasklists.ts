1cf2d456542745ebe974702a19ec09ed83e136db
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-04 16:14:51 +0200

    Refactored tasklists and workspaces stores

--- src/stores/tasklists.ts
+++ src/stores/tasklists.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
-import { ref, Ref } from "vue";
-import { TaskList } from "../interfaces/Workspace";
+import { ref, type Ref } from "vue";
+import { type TaskList } from "../interfaces/Workspace";
 
 export const useTaskLists = defineStore("tasklists", () => {
   const taskLists: Ref<TaskList[]> = ref([]);
@@ -10,22 +10,19 @@ export const useTaskLists = defineStore("tasklists", () => {
   }
 
   function editTaskList(taskList: TaskList) {
-    const index = getTaskListIndexById(taskList.id);
-    taskLists[index] = taskList;
+    const index = taskLists.value.indexOf(taskList);
+
+    taskLists.value[index] = taskList;
   }
 
   function removeTaskList(taskListId: string) {
     taskLists.value.filter((taskList) => taskList.id !== taskListId);
   }
 
-  function getTaskListIndexById(taskListId: string) {
-    return taskLists.value.findIndex((taskList) => taskList.id === taskListId);
-  }
   return {
     taskLists,
     addTaskList,
     editTaskList,
     removeTaskList,
-    getTaskListIndexById,
   };
 });
--- src/stores/workspaces.ts
+++ src/stores/workspaces.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
-import { Workspace } from "../interfaces/Workspace";
-import { Ref, ref } from "vue";
+import { type Workspace } from "../interfaces/Workspace";
+import { type Ref, ref } from "vue";
 
 export const useWorkspacesStore = defineStore("workspaces", () => {
   const workspaces: Ref<Workspace[]> = ref([]);
@@ -11,17 +11,11 @@ export const useWorkspacesStore = defineStore("workspaces", () => {
 
   function editWorkspace(workspace: Workspace) {
     const index = workspaces.value.indexOf(workspace);
-    workspaces[index] = workspace;
+    workspaces.value[index] = workspace;
   }
   function removeWorkspace(id: string) {
     workspaces.value = workspaces.value.filter(
       (workspace) => workspace.id !== id,
-    );
-  }
-
-  function getWorkspaceIndexById(workspaceId: string) {
-    return workspaces.value.findIndex(
-      (workspace) => workspace.id === workspaceId,
     );
   }
 
@@ -30,6 +24,5 @@ export const useWorkspacesStore = defineStore("workspaces", () => {
     addWorkspace,
     editWorkspace,
     removeWorkspace,
-    getWorkspaceIndexById,
   };
 });
