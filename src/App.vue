eb84b33adc306027cd9d7914cc7e011596e10b15
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-05 23:51:02 +0200

    Experimenting

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import SidebarComponent from "./components/sidebar/SidebarComponent.vue";
 import MainPageComponent from "./components/main/MainPageComponent.vue";
+import { useRoute } from "vue-router";
+import { watch, ref } from "vue";
+
+const route = useRoute();
+const currentWorkspaceId = ref(null);
+
+watch(
+  () => route.params.id,
+  (newId, oldId) => {
+    currentWorkspaceId.value = newId;
+  },
+);
 </script>
 
 <template>
   <div id="container">
     <SidebarComponent />
+    <h1>{{ currentWorkspaceId }}</h1>
     <MainPageComponent />
   </div>
 </template>
