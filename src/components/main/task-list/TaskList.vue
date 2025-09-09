45ec439586ce88070d5a68a3d5262e536ab67334
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-09 08:13:54 +0200

    Basic markup and styling for card adding function

--- src/components/main/task-list/TaskList.vue
+++ src/components/main/task-list/TaskList.vue
@@ -4,9 +4,11 @@ import { type TaskList } from "@/interfaces/workspaces";
 import { useTaskListsStore } from "../../../stores/tasklists";
 import { useCardsStore } from "../../../stores/cards";
 import { computed, ref } from "vue";
+import { v4 as uuid } from "uuid";
 
 const props = defineProps<{ taskList: TaskList }>();
 const currentTaskList = computed(() => props.taskList);
+
 const isEditingTitle = ref(false);
 const newTaskListTitle = ref("");
 
@@ -16,6 +18,22 @@ const cardsStore = useCardsStore();
 const currentCards = computed(() =>
   cardsStore.getCardsByTaskListId(currentTaskList.value.id),
 );
+
+const isAddingCard = ref(false);
+const newCardTitle = ref("");
+
+function addCard() {
+  if (!newCardTitle) return;
+
+  cardsStore.addCard({
+    id: uuid(),
+    taskListId: currentTaskList.value.id,
+    title: newCardTitle.value,
+  });
+
+  isAddingCard.value = false;
+  newCardTitle.value = "";
+}
 
 function editTitle() {
   if (!newTaskListTitle) return;
@@ -47,7 +65,14 @@ function editTitle() {
       ></textarea>
     </div>
     <TaskListItem v-for="card in currentCards" />
-    <button class="list-add">+ Add Card</button>
+    <div class="new-card-container">
+      <textarea name="" id="" v-model="newCardTitle"></textarea>
+      <span class="new-card-container-buttons">
+        <button>Add card</button>
+        <span id="close"></span>
+      </span>
+    </div>
+    <button class="list-add" v-if="!isAddingCard">+ Add Card</button>
   </div>
 </template>
 
@@ -91,6 +116,73 @@ function editTitle() {
   padding: 10px;
 
   border-radius: 8px;
+}
+.new-card-container {
+  margin-bottom: 5px;
+
+  display: flex;
+  flex-flow: wrap column;
+
+  gap: 2px;
+  background-color: #fff;
+  border-radius: 6px;
+  padding: 4px 7px;
+}
+.new-card-container-buttons {
+  display: flex;
+  align-items: center;
+  gap: 5px;
+}
+.new-card-container-buttons button {
+  cursor: pointer;
+
+  padding: 6px 40px;
+
+  color: #fff;
+  background-color: #007bff;
+  border: none;
+  border-radius: 5px;
+
+  transition: filter 0.1s ease-in-out;
+}
+#close {
+  cursor: pointer;
+
+  position: relative;
+  user-select: none;
+
+  height: 28px;
+  width: 28px;
+  padding: 6px 10px;
+
+  border-radius: 5px;
+
+  background-color: #ccc;
+
+  transition: filter 0.1s ease-in-out;
+}
+#close:hover {
+  filter: brightness(120%);
+}
+#close:active {
+  filter: brightness(110%);
+}
+#close::before,
+#close::after {
+  content: "";
+  position: absolute;
+  width: 56%;
+  top: 46%;
+  left: 19%;
+  border: 1px solid #fff;
+  border-radius: 5px;
+}
+
+#close::before {
+  transform: rotate(45deg);
+}
+#close::after {
+  transform: rotate(-45deg);
 }
 .list-add {
   cursor: pointer;
