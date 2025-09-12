70a3849ba45b4b92796e6d4ef768a67cbcd10d54
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-12 15:33:20 +0200

    Modal dialog init

---
+++ src/components/main/ui/ModalDialog.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useRefs } from 'vue';
+const props = defineProps<{show: boolean}>();
+const { show } = useRefs(props);
+</script>
+<template>
+  <div class="overlay" v-if="show">
+    <div class="wrapper">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<style scoped>
+.overlay {
+  position: fixed;
+  inset: 0;
+  width: 100%;
+  height: 100vh;
+  opacity: 0.5;
+  background-color: #000;
+}
+</style>
