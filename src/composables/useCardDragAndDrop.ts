1255aba355f154b63d8a79a59aab9f609dea05a8
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-24 17:44:25 +0200

    Added onDrop function to card composable

--- src/composables/useCardDragAndDrop.ts
+++ src/composables/useCardDragAndDrop.ts
@@ -5,6 +5,7 @@ import type { Ref } from "vue";
 export function useCardDragAndDrop(currentCard: Ref<Card>) {
   const cardsStore = useCardsStore();
   const { dragStart, swapItems } = useDragAndDrop();
+
   function startDrag(event: DragEvent) {
     if (!event.dataTransfer) return;
     dragStart(event);
@@ -12,8 +13,17 @@ export function useCardDragAndDrop(currentCard: Ref<Card>) {
     event.dataTransfer.setData("itemOrder", currentCard.value.order.toString());
     event.dataTransfer.setData("itemTaskListId", currentCard.value.taskListId);
   }
+  function onDrop(event: DragEvent) {
+    if (!event.dataTransfer) return;
+    const itemId = event.dataTransfer.getData("itemId");
+    if (!itemId) return;
+    const item = cardsStore.cards.find((item) => item.id === itemId);
+    item && (item.taskListId = currentCard.value.taskListId);
+    swapItems(cardsStore.cards, itemId, currentCard.value.id);
+  }
 
   return {
     startDrag,
+    onDrop,
   };
 }
