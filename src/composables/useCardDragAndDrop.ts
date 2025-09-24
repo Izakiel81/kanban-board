6754ad1e255c9d4fbda184577e62d60fb4cf7943
Maksym Habinskyi <mgabin348@gmail.com>
2025-09-24 17:38:50 +0200

    Added startDrag function to the card composable

--- src/composables/useCardDragAndDrop.ts
+++ src/composables/useCardDragAndDrop.ts
@@ -1,1 +1,19 @@
-export function useCardDragAndDrop() {}
+import { useDragAndDrop } from "./useDragAndDrop";
+import { useCardsStore } from "../stores/cards";
+import type { Card } from "../interfaces/Workspace";
+import type { Ref } from "vue";
+export function useCardDragAndDrop(currentCard: Ref<Card>) {
+  const cardsStore = useCardsStore();
+  const { dragStart, swapItems } = useDragAndDrop();
+  function startDrag(event: DragEvent) {
+    if (!event.dataTransfer) return;
+    dragStart(event);
+    event.dataTransfer.setData("itemId", currentCard.value.id);
+    event.dataTransfer.setData("itemOrder", currentCard.value.order.toString());
+    event.dataTransfer.setData("itemTaskListId", currentCard.value.taskListId);
+  }
+
+  return {
+    startDrag,
+  };
+}
