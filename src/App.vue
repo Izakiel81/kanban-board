540eadfa66c05e871ae425c4b9bfe67c9a8d685a
Maksym Habinskyi <mgabin348@gmail.com>
2025-10-07 00:53:39 +0200

    App component redesign

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,76 @@
 <script setup lang="ts">
+import BurgerIcon from "./components/sidebar/BurgerIcon.vue";
 import SidebarComponent from "./components/sidebar/SidebarComponent.vue";
 import MainPageComponent from "./components/main/MainPageComponent.vue";
 import { useRoute } from "vue-router";
-import { computed, watch } from "vue";
+import { computed, ref } from "vue";
 
 const route = useRoute();
 const currentWorkspaceId = computed(() => route.params.id);
+const sidebarFolded = ref(false);
 </script>
 
 <template>
-  <div id="container">
-    <SidebarComponent :id="currentWorkspaceId" />
-    <MainPageComponent v-if="currentWorkspaceId" :id="currentWorkspaceId" />
+  <div id="container" :class="{ folded: sidebarFolded }">
+    <aside class="sidebar">
+      <SidebarComponent :id="currentWorkspaceId" @fold="sidebarFolded = true" />
+    </aside>
+
+    <main class="main">
+      <div
+        v-if="sidebarFolded"
+        class="burger-icon-container"
+        @click.stop="
+          () => {
+            sidebarFolded = false;
+          }
+        "
+      >
+        <BurgerIcon />
+      </div>
+      <MainPageComponent v-if="currentWorkspaceId" :id="currentWorkspaceId" />
+    </main>
   </div>
 </template>
 
-<style>
+<style scoped>
 #container {
   display: grid;
-  grid-template-columns: 250px 1fr;
+  grid-template-columns: auto 1fr;
+  transition: grid-template-columns 0.3s ease;
+  height: 100vh;
+  background-color: #87c2fd;
+}
+
+.burger-icon-container {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 20px;
+  height: 20px;
+  margin: 10px 15px;
+}
+
+.sidebar {
+  position: relative;
+  left: 2px;
+  width: 250px;
+  overflow: hidden;
+  transition:
+    width 0.3s ease,
+    opacity 0.3s ease;
+  align-self: center;
+}
+
+.folded .sidebar {
+  width: 0;
+  opacity: 0;
+}
+
+.main {
+  overflow-wrap: break-word;
+  transition: padding 0.3s ease;
 }
 </style>
