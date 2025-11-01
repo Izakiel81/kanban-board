20baddffc2537a0fee776658623ef28a2f36d031
Maksym Habinskyi <mgabin348@gmail.com>
2025-11-01 23:46:11 +0100

    Improved findMaxOrder function

--- src/stores/utils/findMaxOrder.ts
+++ src/stores/utils/findMaxOrder.ts
@@ -1,9 +1,4 @@
-import {
-  type Workspace,
-  type TaskList,
-  type Card,
-} from "../../interfaces/Workspace";
-export default function findMaxOrder(arr: Array<Workspace | TaskList | Card>) {
+export default function findMaxOrder(arr: Array<{ order: number }>) {
   let biggestOrder = arr.length ? arr[0].order : 0;
   for (let i = 1; i < arr.length; i++) {
     if (arr[i].order > biggestOrder) biggestOrder = arr[i].order;
