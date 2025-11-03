e20f7358b12a1a906c010de5f4aebf3163d14a57
Maksym Habinskyi <mgabin348@gmail.com>
2025-11-03 16:36:00 +0100

    Implemented refactored AddForm

--- src/components/main/MainPageComponent.vue
+++ src/components/main/MainPageComponent.vue
@@ -2,6 +2,7 @@
 import TaskList from "./task-list/TaskList.vue";
 import { useWorkspacesStore } from "../../stores/workspaces";
 import { useAppStatesStore } from "../../stores/app_store";
+import AddForm from "./ui/AddForm.vue";
 import { ref, computed } from "vue";
 
 const props = defineProps<{ taskLists: Array<any> }>();
@@ -21,11 +22,9 @@ const currentTaskLists = computed(() =>
   props.taskLists.sort((a, b) => a.order - b.order),
 );
 
-function addTaskList() {
-  if (!newTaskListTitle.value || !newTaskListTitle.value.trim()) return;
-  boardsStore.addTaskList(appStates.currentBoardId, newTaskListTitle.value);
-  isAdding.value = false;
-  newTaskListTitle.value = "";
+function addTaskList(newTaskListTitle: string) {
+  if (!newTaskListTitle || !newTaskListTitle.trim()) return;
+  boardsStore.addTaskList(appStates.currentBoardId, newTaskListTitle);
 }
 </script>
 
@@ -44,20 +43,12 @@ function addTaskList() {
       :taskList="taskList"
       :taskLists="taskLists"
     />
-    <div class="new-list-container" v-if="isAdding">
-      <input v-model="newTaskListTitle" />
-      <span class="new-list-container-buttons">
-        <button @click="addTaskList()">Add</button>
-        <span
-          id="close"
-          @click="
-            () => {
-              ((isAdding = false), (newTaskListTitle = ''));
-            }
-          "
-        />
-      </span>
-    </div>
+    <AddForm
+      v-if="isAdding"
+      :onClose="() => (isAdding = false)"
+      :onClick="addTaskList"
+      :wBackground="true"
+    />
     <div class="add-list-container" @click="isAdding = true" v-else>
       <h1>+ Add list</h1>
     </div>
@@ -79,96 +70,6 @@ function addTaskList() {
 
   padding: 10px;
   gap: 2px;
-}
-.new-list-container {
-  display: flex;
-  flex-direction: column;
-
-  gap: 5px;
-
-  padding: 5px;
-
-  min-width: 250px;
-
-  background-color: #0000005f;
-
-  border-radius: 3px;
-}
-.new-list-container input {
-  outline: none;
-
-  border: 2px solid #fff;
-  border-radius: 3px;
-
-  padding: 2px 5px;
-
-  transition: border-color 0.3s ease-out;
-}
-
-.new-list-container input:focus {
-  border-color: #007bff;
-}
-.new-list-container-buttons {
-  display: flex;
-  align-items: center;
-  gap: 5px;
-}
-.new-list-container-buttons button {
-  cursor: pointer;
-
-  padding: 6px 40px;
-
-  color: #fff;
-  background-color: #007bff;
-  border: none;
-  border-radius: 2px;
-
-  transition: filter 0.1s ease-in-out;
-}
-.new-list-container-buttons button:hover {
-  filter: brightness(90%);
-}
-.new-list-container-buttons button:active {
-  filter: brightness(80%);
-}
-#close {
-  cursor: pointer;
-
-  position: relative;
-  user-select: none;
-
-  height: 28px;
-  width: 28px;
-  padding: 6px 10px;
-
-  border-radius: 5px;
-
-  background-color: #555;
-
-  transition: filter 0.1s ease-in-out;
-}
-#close:hover {
-  filter: brightness(120%);
-}
-#close:active {
-  filter: brightness(110%);
-}
-#close::before,
-#close::after {
-  content: "";
-  position: absolute;
-  width: 56%;
-  top: 46%;
-  left: 20%;
-  border: 1px solid #aaa;
-  border-radius: 5px;
-}
-
-#close::before {
-  transform: rotate(45deg);
-}
-#close::after {
-  transform: rotate(-45deg);
 }
 
 .add-list-container {
